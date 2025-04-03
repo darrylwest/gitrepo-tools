d0c15d9b6375f343df5fc343752818484bc266db
darryl west <darryl.west@raincitysoftware.com>
2025-04-03 07:31:40 -0700

    jobs

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include <gitrepo/config.hpp>
 #include <gitrepo/scanner.hpp>
 #include <gitrepo/tools.hpp>
+#include <future>
+#include <thread>
+#include <vector>
 
 
 int main(int argc, char **argv) {
@@ -29,14 +32,29 @@ int main(int argc, char **argv) {
     auto folders = gitrepo::scanner::scan_folders(config);
     spdlog::info("folder count: {}", folders.size());
 
+    std::vector<std::future<std::string>> jobs;
+    jobs.reserve(folders.size());
     for (const auto& folder : folders) {
         spdlog::debug("folder: {}", folder.c_str());
         const auto repo = gitrepo::tools::scan_repo(folder);
         spdlog::info("repo: {}", repo.to_json().dump());
 
+        if (repo.status != "clean") {
+            continue;
+        }
+
         // execute the command in a separate thread
+        jobs.push_back(std::async(std::launch::async, gitrepo::tools::exec, repo.parent, ctx.cmd));
     }
 
+    std::vector<std::string> results;
+    for (auto& job : jobs) {
+        results.push_back(job.get());
+    }
+
+    for (const auto& result : results) {
+        spdlog::info("{}", result);
+    }
 
     return 0;
 }
