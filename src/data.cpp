c65eecc238eba0066836ef6e01a995a903460ec4
darryl west <darryl.west@raincitysoftware.com>
2025-04-04 22:10:44 -0700

    updated for database

--- src/data.cpp
+++ src/data.cpp
@@ -12,31 +12,31 @@
 #include <vector>
 #include <quickkv/quickkv.hpp>
 #include <domainkeys/keys.hpp>
+#include <filesystem>
 
 namespace gitrepo::data {
     using json = nlohmann::json;
+    namespace fs = std::filesystem;
 
     // read the data file, parse the json and return a vector of GitRepo objects
     std::vector<tools::GitRepo> read_repos_db(const std::string& datafile) {
-
         spdlog::info("reading repos from {}", datafile);
         std::vector<tools::GitRepo> repos;
-        // read from the database
-        std::ifstream json_file(datafile);
-        if (!json_file || !json_file.is_open()) {
-            spdlog::error("failed to open repo database {}", datafile);
-            spdlog::info("return the empty vector");
+        auto path = fs::path(datafile);
 
-            return repos;
-        }
-
-        json jdata;
-        json_file >> jdata;
-        json_file.close();
+        quickkv::KVStore store;
+        store.read(path);
 
         // now create the repos vector
-        for (const json& item : jdata) {
-            repos.emplace_back(tools::from_json(item));
+        for (const auto& key : store.keys()) {
+            spdlog::debug("reading key {}", key);
+            auto response = store.get(key);
+            if (response) {
+                auto jval = json::parse(response.value());
+                repos.emplace_back(tools::from_json(jval));
+            } else {
+                spdlog::error("response is bad");
+            }
         }
 
         return repos;
@@ -49,7 +49,7 @@ namespace gitrepo::data {
         quickkv::KVStore store;
         for (const tools::GitRepo& repo : repos) {
             auto key = domainkeys::keys::create_timestamp_key();
-            store.set(key.to_string(), repo.to_json().dump(4));
+            store.set(key.to_string(), repo.to_json().dump());
         }
 
         if (store.write(datafile)) {
